f92c2209b4947bfd64f1027d501b28fb7ffab21e
jshleap <jshleap@squalus.org>
2017-10-31 17:23:15 -0400

    Adding matric operations in place of loops for efficiency

--- ExpectedSquaredEffect2.py
+++ ExpectedSquaredEffect2.py
@@ -7,6 +7,7 @@ import argparse
 import numexpr
 import numpy as np
 import pickle
+import mmap
 from tqdm import tqdm
 import matplotlib
 matplotlib.use('Agg')
@@ -18,15 +19,27 @@ from joblib import delayed, parallel
 plt.style.use('ggplot')
 
 #----------------------------------------------------------------------
+def mapcount(filename):
+    """
+    Efficient line counter courtesy of Ryan Ginstrom answer in stack overflow
+    """
+    f = open(filename, "r+")
+    buf = mmap.mmap(f.fileno(), 0)
+    lines = 0
+    readline = buf.readline
+    while readline():
+        lines += 1
+    return lines    
+
+#----------------------------------------------------------------------
 def vs(B_hat_x, N, h2_l):
     """
     compute v_x
     """
-    return (( N * B_hat_x ) / (2 * ( 1 - h2_l ) ))
-    
+    return (( N * B_hat_x ) / (2 * ( 1 - h2_l ) ))   
     
 #----------------------------------------------------------------------
-def integral(vs, mu, snps):
+def integral_b(vs, mu, snps):
     """
     Compute the expected beta square
     :param vs: vector of v
@@ -35,8 +48,10 @@ def integral(vs, mu, snps):
     :param N: Number of individuals
     :param snps: names of snps in order
     """
-    exp = np.exp( (vs**2) / (4 * mu) )
-    lhs = ( (2 * mu) + (vs**2) ) / ( 4 * (mu**2) )
+    snps = snps.tolist()
+    exp = np.exp( np.power(vs,2) / (4 * mu) )
+    exp.index = snps
+    lhs = ( (2 * mu) + np.power(vs,2) ) / ( 4 * np.power(mu,2) )
     rhs = exp / exp.sum()
     vec = lhs * rhs
     vec.index = snps
@@ -98,18 +113,61 @@ def read_lds(refld, tarld):
     """
     read LD matrix
     """
-    if not os.path.isfile('referenceLD.pickle'):
-        refld = pd.read_table(refld, delim_whitespace=True)
-        refld.to_pickle('referenceLD.pickle')#, compression='gzip')
-    else:
-        refld = pd.read_pickle('referenceLD.pickle', compression='gzip')
-    if not os.path.isfile('targetLD.pickle'):
-        tarld = pd.read_table(tarld, delim_whitespace=True)
-        tarld.to_pickle('targetLD.pickle')#, compression='gzip')
-    else:
-        tarld = pd.read_pickle('targetLD.pickle')#, compression='gzip')
+    dtypes = {'SNP_A':str, 'SNP_B':str, 'D':float}
+    #if not os.path.isfile('referenceLD.pickle'):
+    print('Reading reference ld from %s' % refld)
+    refld = pd.read_table(refld, engine='c', delim_whitespace=True, 
+                          usecols=['SNP_A', 'SNP_B', 'D'], dtype=dtypes,
+                          memory_map=True)
+    #    refld.to_pickle('referenceLD.pickle')#, compression='gzip')
+    #else:
+        #refld = pd.read_pickle('referenceLD.pickle')#, compression='gzip')
+    #if not os.path.isfile('targetLD.pickle'):
+    print('Reading target ld from %s' % tarld)
+    tarld = pd.read_table(tarld, engine='c', delim_whitespace=True,
+                          usecols=['SNP_A', 'SNP_B', 'D'], dtype=dtypes,
+                          memory_map=True)
+        #tarld.to_pickle('targetLD.pickle')#, compression='gzip')
+    #else:
+        #tarld = pd.read_pickle('targetLD.pickle')#, compression='gzip')
+    #intersect the LDs
+    refsnps = set(refld.loc[:,['SNP_A', 'SNP_B']].unstack())
+    tarsnps = set(tarld.loc[:,['SNP_A', 'SNP_B']].unstack())
+    available_snps = list(refsnps.intersection(tarsnps))
+    refld = refld[refld.SNP_A.isin(available_snps) & 
+                  refld.SNP_B.isin(available_snps)]
+    tarld = tarld[tarld.SNP_A.isin(available_snps) & 
+                  tarld.SNP_B.isin(available_snps)]    
     return refld, tarld
-
+#----------------------------------------------------------------------
+def get_all_is(ld1, ld2, avail, integral):
+    """
+    Given a locus' SNPs, compute the expected covariance for all is
+    """
+    # Fetch D info for the locus
+    query = ('((SNP_A == @avail) | (SNP_B == @avail))')
+    ld1 = ld1.query(query)
+    ld2 = ld2.query(query)
+    # symetrize ld matrices
+    D1 = pd.DataFrame(ld1[ld1.SNP_A.isin(avail) & ld1.SNP_B.isin(avail)].pivot(
+        columns='SNP_B', index='SNP_A', values='D'), index=avail, 
+                      columns=avail).fillna(0)
+    #D1.update(piv1)
+    D1 = pd.DataFrame((np.triu(D1) + np.triu(D1,1).T), index=D1.index, 
+                      columns=D1.columns)
+    np.fill_diagonal(D1.values, 1) 
+    D2 = pd.DataFrame(ld2[ld2.SNP_A.isin(avail) & ld2.SNP_B.isin(avail)].pivot(
+        columns='SNP_B', index='SNP_A', values='D'), index=avail, 
+                      columns=avail).fillna(0)
+    #D2.update(piv2)
+    D2 = pd.DataFrame((np.triu(D2) + np.triu(D2, 1).T), index=D2.index, 
+                      columns=D2.columns)
+    np.fill_diagonal(D2.values, 1)
+    # compute the dot product for the locus
+    ec_i = (D1 * D2).dot(integral).to_frame(name='ese')
+    ec_i['SNP'] = ec_i.index.tolist() 
+    return ec_i
+    
 #----------------------------------------------------------------------
 def get_Ds(ld1, ld2, i, avail, integral):
     """
@@ -146,7 +204,7 @@ def ese(i, j, M, all_markers, N, h2_l, denominator, mu, ld1, ld2):
     """
     compute single expected value of i over js
     """
-    b2 = (integral(j.BETA, M, all_markers, N, h2_l, mu) / 
+    b2 = (integral_b(j.BETA, M, all_markers, N, h2_l, mu) / 
           denominator )
     dp = get_D(ld1, i.SNP, j.SNP) * get_D(ld2, i.SNP, j.SNP)
     return dp * b2   
@@ -158,14 +216,15 @@ def per_locus(locus, sumstats, avh2, h2,  N, ld1, ld2):
     """
     locus = sumstats[sumstats.SNP.isin(locus)].reset_index(drop=True)
     locus = locus.astype({'BETA':np.longfloat})
+    locus.index = locus.SNP.tolist()
     M = locus.shape[0]
     h2_l = avh2 * M
     mu = ( (N /(2 * ( 1 - h2_l ) ) ) + ( M / ( 2 * h2 ) ) )
-    vs = np.exp( ((( N * locus.BETA ) / (2 * ( 1 - h2_l ) ))**2)/(4 * mu) )
-    vs.index = locus.SNP.tolist()
-    I = integral(vs, mu, locus.SNP)
-    expcovs = [get_Ds(ld1, ld2, i, locus.SNP.tolist(), I) for i in locus.SNP]
-    return pd.DataFrame({'SNP':locus.SNP, 'ese':expcovs})
+    vjs = (( N * locus.BETA ) / (2 * ( 1 - h2_l ) ))
+    I = integral_b(vjs, mu, locus.SNP)
+    expcovs = get_all_is(ld1, ld2, sorted(locus.SNP.tolist()), I)
+    #expcovs = [get_Ds(ld1, ld2, i, locus.SNP.tolist(), I) for i in locus.SNP]
+    return expcovs
         
 #----------------------------------------------------------------------
 def transferability(args):
@@ -182,38 +241,10 @@ def transferability(args):
     with open('loci.pickle','wb') as L:
         pickle.dump(loci, L)
     #res = pd.DataFrame(columns=['SNP', 'ese'])
-    N = args.N
+    N = mapcount('%s.fam' % args.reference)
     res = Parallel(n_jobs=int(args.threads))(delayed(per_locus)(
-        locus, sumstats, avh2, h2, N, ld1, ld2) for index, locus in tqdm(
-            loci.items(), total=len(loci)))    
-    
-    #for index, locus in tqdm(loci.items(), total=len(loci)):
-        ##im = np.zeros((len(locus), len(locus)))
-        #locus = sumstats[sumstats.SNP.isin(locus)].reset_index(drop=True)
-        #M = locus.shape[0]
-        #print('Processing locus %s with M=%d' %(index, M))
-        #h2_l = avh2 * M
-        #mu = ( (N /(2 * ( 1 - h2_l ) ) ) + ( M / ( 2 * h2 ) ) )
-        #vs = np.exp(((( N * locus.BETA ) / (2 * ( 1 - h2_l ) ))**2)/(4 * mu))
-        #I = integral(vs, mu, locus.SNP)
-        #expcovs = [get_Ds(ld1, ld2, i, locus.SNP) for i in locus.SNP]
-        #denominator = np.sum([np.exp((vs(k, N, h2_l)**2)/(4 * mu)) for k in 
-        #                      locus.BETA])
-        #if not os.path.isfile('vec_%s.pickle'%index):
-            #vec = [ese(i, j, M, all_markers, N, h2_l, denominator, mu, ld1, ld2) 
-                   #for i, j in permutations(locus.itertuples(), 2)]
-            #with open('vec_%s.pickle'%index, 'wb') as v:
-                #pickle.dump(vec, v)
-        #else:
-            #with open('vec_%s.pickle'%index, 'rb') as v:
-                #vec = pickle.load(v)
-        #vec =  np.apply_along_axis(sum, 1, np.split(np.array(vec), M-1))
-        #vec = [np.sum([ese(i, j, M, all_markers, N, h2_l, denominator,
-        #                   mu, ld1, ld2)
-        #               for j in locus.itertuples() if j != i]) 
-        #       for i in locus.itertuples()
-        #       ]
-        #res = res.append(pd.DataFrame({'SNP':locus.SNP, 'ese':expcovs}))
+        locus, sumstats, avh2, h2, N, ld1, ld2) for locus in tqdm(
+            loci.values(), total=len(loci)))    
     res = pd.concat(res)
     product, _ = smartcotagsort(args.prefix, res, column='ese')
     nsnps = product.shape[0]
@@ -225,8 +256,8 @@ def transferability(args):
     product.loc[:,['SNP', 'Index']].to_csv(qfile, sep=' ', header=False,
                                            index=False)   
     df = qrscore(args.plinkexe, args.target, args.sumstats, qrange, qfile, 
-                 args.pheno, args.prefix, qr, args.maxmem, args.threads, 'None', 
-                 args.prefix)
+                 args.pheno, args.prefix, qr, args.maxmem, args.threads, 
+                 'None', args.prefix)
     #get ppt results
     ppts=[]
     for i in glob('*.results'):
@@ -265,8 +296,8 @@ if __name__ == '__main__':
                                                    'file '))
     parser.add_argument('-s', '--sumstats', help='Filename of sumstats',
                         required=True)
-    parser.add_argument('-n', '--N', type=int, help='Number of individuals',
-    required=True)
+    #parser.add_argument('-n', '--N', type=int, help='Number of individuals',
+    #required=True)
     parser.add_argument('-H', '--h2', type=float, help='Heritability of trait',
                         required=True)
     parser.add_argument('-r', '--rmap', help='Path to recombination map',
