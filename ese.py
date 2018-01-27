including causal prunning and fixing plots

ese.py
@@ -1,15 +1,9 @@
 """
 Expected beta
 """
-import argparse
 import matplotlib
 matplotlib.use('Agg')
-import pandas as pd
-import numpy as np
 import gzip
-import matplotlib.pyplot as plt
-from utilities4cotagging import *
-from qtraitsimulation import *
 from prankcster import *
 from plinkGWAS import *
 from joblib import delayed, Parallel
@@ -87,7 +81,7 @@ def integral_b(vs, mu, snps):
     :param mu: mean
     :param snps: names of snps in order
     """
-    clipped = np.clip((vs * vs) / (4 * mu), a_max=709.7)
+    clipped = np.clip((vs * vs) / (4 * mu), 0, 709.7)
     exp = np.exp(clipped, dtype=np.longfloat)
     lhs = ((2 * mu) + (vs * vs)) / (4 * (mu * mu))
     rhs = exp / exp.sum()
@@ -96,7 +90,7 @@ def integral_b(vs, mu, snps):
 
 
 # ----------------------------------------------------------------------
-#@jit
+@jit
 def per_locus(locus, sumstats, avh2, h2, n, within=False, integral_only=False):
     """
     compute the per-locus expectation
@@ -111,7 +105,7 @@ def per_locus(locus, sumstats, avh2, h2, n, within=False, integral_only=False):
     I = integral_b(vjs, mu, snps)
     assert np.all(I >= 0)  # make sure integral is positive
     if integral_only:
-        return pd.DataFrame({'snp': snps, 'ese': I})
+        return pd.DataFrame({'snp': I.index, 'ese': I.values})
     assert max(I) > 0 # check if at least one is different than 0
     if within == 1:
         expcovs = (D_r * D_r).dot(I)

utilities4cotagging.py
@@ -326,8 +326,6 @@ def helper_smartsort2(grouped, key):
 def read_geno(bfile, freq_thresh, threads, flip=False, memory=None):
 
     (bim, fam, G) = read_plink(bfile)
-    #chunks = estimate_chunks(G.shape, threads, memory=memory)
-    #G = da.rechunk(G, chunks=chunks)
     # remove constant variants
     G_std = G.std(axis=1).compute(num_workers=threads)
     m, n = G.shape
@@ -379,7 +377,7 @@ def smartcotagsort(prefix, gwascotag, column='Cotagging', ascending=False):
             df, beforetail = pickle.load(F)
     else:
         print('Sorting File based on %s "clumping"...' % column)
-        gwascotag['size'] = norm(abs(gwascotag.slope), 5, 20)
+        gwascotag['m_size'] = norm(abs(gwascotag.slope), 5, 20)
         grouped = gwascotag.sort_values(by=column, ascending=ascending).groupby(
             column, as_index=False).first()
         sorteddf = grouped.sort_values(by=column, ascending=ascending)
@@ -387,17 +385,18 @@ def smartcotagsort(prefix, gwascotag, column='Cotagging', ascending=False):
         beforetail = sorteddf.shape[0]
         df = sorteddf.copy()
         if not tail.empty:
-            df = df.append(tail.sample(frac=1))
+            df = df.append(tail.sample(frac=1), ignore_index=True)
         df = df.reset_index(drop=True)
         df['index'] = df.index.tolist()
-        #df['gen_index'] = df.index.tolist()
         with open(picklefile, 'wb') as F:
             pickle.dump((df, beforetail), F)
     f, ax = plt.subplots()
-    df.plot.scatter(x='index', y='pos', ax=ax, label=column)
-    df.dropna.plot.scatter(x='index', y='pos', m='*', s=df.size, ax=ax,
-                           label='Causals')
-    plt.savefig('%s.pdf' % '_'.join(column.split()))
+    df.plot.scatter(x='pos', y='index', ax=ax, label=column)
+    df.dropna(subset=['beta']).plot.scatter(x='pos', y='index', marker='*',
+                                            s=df.m_size, ax=ax, label='Causals',
+                                            c='k')
+    plt.tight_layout()
+    plt.savefig('%s_%s.pdf' % (prefix, '_'.join(column.split())))
     return df, beforetail
 
 