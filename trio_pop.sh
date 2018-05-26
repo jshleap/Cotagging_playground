8284295f633a29fba7bee46b68d0398d2b737eb6
jshleap <jshleap@squalus.org>
2018-05-26 15:09:21 -0400

    Including an extra PC

--- trio_pop.sh
+++ trio_pop.sh
@@ -123,7 +123,7 @@ fi
 
 if [ ! -f train.eigenvec  ]
     then
-        cut -f1,2,6,7,8 ${genos}/pca_proj_mydata.sscore| tail -n +2 > train.eigenvec
+        cut -f1,2,5,6,7,8 ${genos}/pca_proj_mydata.sscore| tail -n +2 > train.eigenvec
 fi
 
 step=$(( sample/10 ))
