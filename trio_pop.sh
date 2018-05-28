ad010cce6f2a079d1078edc58badd6993ae9e1e2
jshleap <jshleap@gmail.com>
2018-05-28 15:16:52 -0400

    Include header in PCA

--- trio_pop.sh
+++ trio_pop.sh
@@ -132,7 +132,8 @@ fi
 
 if [ ! -f train.eigenvec  ]
     then
-        cut -f1,2,5,6,7,8 ${genos}/pca_proj_mydata.sscore| tail -n +2 > train.eigenvec
+        #cut -f1,2,5,6,7,8 ${genos}/pca_proj_mydata.sscore| tail -n +2 > train.eigenvec
+        cut -f1,2,5,6,7,8 ${genos}/pca_proj_mydata.sscore > train.eigenvec
         #$plink --bfile ${all} --pca 4 --out train ${common_plink}
 fi
 
